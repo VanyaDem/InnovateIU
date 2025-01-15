4e9ef6cc421875568a7ef55df18df2320a20f3a9
Ivan <vanyademglo@gmail.com>
2025-01-15 05:58:33 +0200

    completed the task

--- src/main/java/com/InnovateIU/DocumentManager.java
+++ src/main/java/com/InnovateIU/DocumentManager.java
@@ -1,14 +1,16 @@
+package com.InnovateIU;
+
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.Instant;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * For implement this task focus on clear code, and make this solution as simple readable as possible
- * Don't worry about performance, concurrency, etc
+ * Don't worry about performance, concurrency, etc.
  * You can use in Memory collection for sore data
  * <p>
  * Please, don't change class name, and signature for methods save, search, findById
@@ -16,6 +18,8 @@ import java.util.Optional;
  * This class could be auto tested
  */
 public class DocumentManager {
+
+    private final Map<String, Document> storage = new HashMap<>();
 
     /**
      * Implementation of this method should upsert the document to your storage
@@ -25,8 +29,8 @@ public class DocumentManager {
      * @return saved document
      */
     public Document save(Document document) {
-
-        return null;
+        DocumentSaver saver = DocumentSaverFactory.getInstance().getDocumentSaver(document);
+        return saver.saveDocument(document, storage); // Polymorphism GRASP pattern
     }
 
     /**
@@ -36,8 +40,11 @@ public class DocumentManager {
      * @return list matched documents
      */
     public List<Document> search(SearchRequest request) {
-
-        return Collections.emptyList();
+        return storage
+                .values()
+                .stream()
+                .filter(document -> isDocumentMatchingRequest(document, request))
+                .collect(Collectors.toList());
     }
 
     /**
@@ -47,8 +54,30 @@ public class DocumentManager {
      * @return optional document
      */
     public Optional<Document> findById(String id) {
+        Document document = storage.get(id);
+        return Optional.of(document);
+    }
 
-        return Optional.empty();
+    private boolean isDocumentMatchingRequest(Document document, SearchRequest request) {
+        return Optional.of(document)
+                .filter(d -> isMatching(request.getTitlePrefixes(), d.getTitle()::startsWith))
+                .filter(d -> isMatching(request.getContainsContents(), d.getContent()::contains))
+                .filter(d -> isMatching(request.getAuthorIds(), d.getAuthor().getId()::equals))
+                .filter(d -> isCreatedBefore(d.getCreated(), request.getCreatedTo()))
+                .filter(d -> isCreatedAfter(d.getCreated(), request.getCreatedFrom()))
+                .isPresent();
+    }
+
+    private boolean isMatching(List<String> requestsAttributes, Predicate<String> predicate) {
+        return requestsAttributes == null || requestsAttributes.stream().anyMatch(predicate);
+    }
+
+    private boolean isCreatedBefore(Instant created, Instant to) {
+        return to == null || created.isBefore(to);
+    }
+
+    private boolean isCreatedAfter(Instant created, Instant from) {
+        return from == null || created.isAfter(from);
     }
 
     @Data
@@ -76,5 +105,46 @@ public class DocumentManager {
     public static class Author {
         private String id;
         private String name;
+    }
+
+    /**
+     * Defines a method for saving a document to storage.
+     * This is part of the Polymorphism GRASP pattern, as different
+     * implementations handle the document-saving behavior depending on the state of the document.
+     */
+    public interface DocumentSaver {
+        Document saveDocument(Document document, Map<String, Document> storage);
+    }
+
+    public static class NewDocumentSaver implements DocumentSaver {
+        @Override
+        public Document saveDocument(Document document, Map<String, Document> storage) {
+            String id = UUID.randomUUID().toString();
+            document.setId(id);
+            storage.put(id, document);
+            return document;
+        }
+    }
+
+    public static class ExistedDocumentSaver implements DocumentSaver {
+        @Override
+        public Document saveDocument(Document document, Map<String, Document> storage) {
+            return storage.put(document.getId(), document);
+        }
+    }
+
+    public static class DocumentSaverFactory {
+
+        private static final DocumentSaverFactory INSTANCE = new DocumentSaverFactory();
+
+        private DocumentSaverFactory() {}
+
+        public static DocumentSaverFactory getInstance() {
+            return INSTANCE;
+        }
+
+        public DocumentSaver getDocumentSaver(Document document) {
+            return document.getId() == null ? new NewDocumentSaver() : new ExistedDocumentSaver();
+        }
     }
 }
--- src/test/java/com/InnovateIU/DocumentManagerTest.java
+++ src/test/java/com/InnovateIU/DocumentManagerTest.java
@@ -1,4 +1,200 @@
-import junit.framework.TestCase;
-public class DocumentManagerTest extends TestCase {
-  
+package com.InnovateIU;
+
+import com.InnovateIU.DocumentManager.Document;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+
+import static com.InnovateIU.DocumentManager.Author;
+import static com.InnovateIU.DocumentManager.SearchRequest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DocumentManagerTest {
+
+    DocumentManager manager;
+
+    Map<String, Document> storage;
+
+
+    @SuppressWarnings("unchecked")
+    @SneakyThrows
+    @BeforeEach
+    public void setUp() {
+        manager = new DocumentManager();
+
+        Field field = manager.getClass().getDeclaredField("storage");
+        field.setAccessible(true);
+
+        storage = (Map<String, Document>) field.get(manager);
+
+        Document document = Document
+                .builder()
+                .id("123")
+                .title("Some title")
+                .content("Some content")
+                .author(Author
+                        .builder()
+                        .id("1")
+                        .name("Ivan")
+                        .build())
+                .created(Instant.now())
+                .build();
+
+        storage.put("123", document);
+    }
+
+    @Test
+    public void saveShouldSaveNewDocument() {
+        Document document = generateDocument();
+        Document savedDocument = manager.save(document);
+
+        assertEquals(2, storage.size());
+        assertEquals("Some content 123", storage.get(savedDocument.getId()).getContent());
+    }
+
+    @Test
+    public void saveShouldUpdateDocument() {
+        Document document = storage.get("123");
+        document.setContent(document.getContent() + " 456");
+        Document savedDocument = manager.save(document);
+
+        assertEquals(1, storage.size());
+        assertEquals("Some content 456", storage.get("123").getContent());
+        assertEquals("Some content 456", savedDocument.getContent());
+    }
+
+    @Test
+    public void searchShouldReturnListOfDocumentsByTitles() {
+        var titlePrefixes = List.of("So", "title");
+        var request = SearchRequest.builder().titlePrefixes(titlePrefixes).build();
+        var documents = generateListOfDocuments();
+        documents.forEach(d -> storage.put(d.getId(), d));
+
+        var resultList = manager.search(request);
+
+        assertEquals(3, resultList.size());
+
+    }
+
+    @Test
+    public void searchShouldReturnListOfDocumentsByContents() {
+        var contents = List.of("So", "twu");
+        var request = SearchRequest.builder().containsContents(contents).build();
+        var documents = generateListOfDocuments();
+        documents.forEach(d -> storage.put(d.getId(), d));
+
+        var resultList = manager.search(request);
+
+        assertEquals(5, resultList.size());
+
+    }
+
+    @Test
+    public void searchShouldReturnListOfDocumentsByAuthor() {
+        var authorIds = List.of("11", "2");
+        var request = SearchRequest.builder().authorIds(authorIds).build();
+        var documents = generateListOfDocuments();
+        documents.forEach(d -> storage.put(d.getId(), d));
+
+        var resultList = manager.search(request);
+
+        assertEquals(1, resultList.size());
+        assertEquals("title 111", resultList.get(0).getTitle());
+        assertEquals("3", resultList.get(0).getId());
+
+    }
+
+    @Test
+    public void searchShouldReturnListOfDocumentsCreatedUntilGivenTime() {
+        Instant to = Instant.parse("2024-01-15T13:00:00Z");
+        var request = SearchRequest.builder().createdTo(to).build();
+        var documents = generateListOfDocuments();
+        documents.forEach(d -> storage.put(d.getId(), d));
+
+        var resultList = manager.search(request);
+
+        assertEquals(1, resultList.size());
+        assertEquals("Some", resultList.get(0).getTitle());
+        assertEquals("1", resultList.get(0).getId());
+
+    }
+
+    @Test
+    public void searchShouldReturnListOfDocumentsCreatedFromGivenTime() {
+        Instant from = Instant.parse("2024-01-15T13:00:00Z");
+        var request = SearchRequest.builder().createdFrom(from).build();
+        var documents = generateListOfDocuments();
+        documents.forEach(d -> storage.put(d.getId(), d));
+
+        var resultList = manager.search(request);
+
+        assertEquals(4, resultList.size());
+
+    }
+
+    @Test
+    public void searchShouldReturnNoOne() {
+        Instant from = Instant.parse("2024-01-15T13:00:00Z");
+        var request = SearchRequest.builder()
+                .createdFrom(from)
+                .titlePrefixes(List.of("Bla Bla"))
+                .build();
+        var documents = generateListOfDocuments();
+        documents.forEach(d -> storage.put(d.getId(), d));
+
+        var resultList = manager.search(request);
+
+        assertEquals(0, resultList.size());
+
+    }
+
+
+    @Test
+    public void findByIdShouldReturnDocumentWithSpecifiedId() {
+        assertTrue(manager.findById("123").isPresent());
+    }
+
+    private static Document generateDocument() {
+        return Document
+                .builder()
+                .id(null)
+                .title("Some title")
+                .content("Some content 123")
+                .author(Author
+                        .builder()
+                        .id("1")
+                        .name("Ivan")
+                        .build())
+                .created(Instant.now())
+                .build();
+    }
+
+    private static List<Document> generateListOfDocuments() {
+        var d0 = generateDocument();
+        d0.setId("1");
+        d0.setTitle("Some");
+        d0.setCreated(Instant.parse("2023-01-15T13:00:00Z"));
+
+        var d1 = generateDocument();
+        d1.setId("2");
+        d1.setTitle("Title");
+
+        var d2 = generateDocument();
+        d2.setId("3");
+        d2.setAuthor(Author.builder().id("2").name("Ann").build());
+        d2.setTitle("title 111");
+
+        var d3 = generateDocument();
+        d3.setId("4");
+        d3.setContent("One twu three");
+        d3.setTitle("1");
+        return List.of(d0, d1, d2, d3);
+    }
+
 }
